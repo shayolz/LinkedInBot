b95f07b1436f13e6019e0582a061c2583fe2f45b
shayolz <enrisilvano@gmail.com>
2021-07-23 18:03:17 +0200

    Import .NET 5 Console application project.

---
+++ LinkedInBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedInBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
